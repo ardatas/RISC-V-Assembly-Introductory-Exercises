f4820a8c9cff476ce70cc98e1d6a72c81c43db01
Arda <85059535+ardatas@users.noreply.github.com>
2025-12-22 23:55:40 +0100

    Improved max_of_arr solution

--- max_of_arr/get_max_solution.s
+++ max_of_arr/get_max_solution.s
@@ -3,15 +3,19 @@
 .org 0x400
 .data
 
-arr1: .word 123, -11, 10, 431, 200
+arr1: .word 123, -11, 10, 1000, 200
 
 .org 0x200
 .text
 
 _start:
-la a0, arr1
-addi a1, zero, 5
-jal ra, get_max
+    la a0, arr1             ; pointer to array 
+    addi a1, zero, 5        ; array length
+
+    lw t0, 0(a0)            ; initialize first arr element for comparison
+    addi a0, a0, 4          ; move pointer to next element
+    addi a1, a1, -1         ; remaining length 
+    jal ra, get_max
 
 ebreak
 
@@ -22,24 +26,21 @@ ebreak
 #   a0: maximum 
 
 get_max:
+    beq a1, zero, end_loop
+    lw t1, 0(a0)            ;load first value of array
+    
+    bge t1, t0, update_max
 
+    addi a0, a0, 4 		    ; move the pointer 
+    addi a1, a1, -1         
+    j get_max;
 
-lw t0, 0(a0) ;load first value of array
-
-
-loop:
-beq a1, zero, end_loop
-lw t1, 0(a0)
-
-;change max if new value (t1) greater or equal than current max (t0)
-bge t0, t1, no_change
-add t0, t1, zero
-
-no_change:
-addi a1, a1, -1 ; decrease length by 1
-addi a0, a0, 4 ; increase pointer by 4 (32 Bits, 4 Bytes) for a new value 
-j loop
+update_max:
+    mv t0, t1
+    addi a0, a0, 4          ; move the pointer 
+    addi a1, a1, -1         
+    j get_max;
 
 end_loop:
-add a0, t0, zero
-ret
+    mv a0, t0
+    ret
